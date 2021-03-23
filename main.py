1454a50f0cf8b549c8515a05560738d8aa92e03e
LukeOL <luke.oleary@mycit.ie>
2021-03-23 18:44:10 +0000

    added requirements

--- main.py
+++ main.py
@@ -13,12 +13,9 @@ from flask import abort
 from sqlalchemy import Table, Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from environs import Env
 
-env = Env()
-env.read_env()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = env('APP_SECRET_KEY')
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
